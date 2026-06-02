8ea45f29f69087120ce74568ecf768e92c2d29b0
louiss0 <louisshelton0@gmail.com>
2026-06-02 12:44:41 -0400

    test(pi-agent-resource): make sure create and delete commands work

--- pi-agent-resource/extensions/pack.test.ts
+++ pi-agent-resource/extensions/pack.test.ts
@@ -8,7 +8,50 @@ function createTestContext(
 }
 
 describe("Pack", () => {
-  describe("Testing rootPackResourceReducer", () => {});
+  describe("Testing rootPackResourceReducer", () => {
+    it("creates a pack when create is passed in", () => {
+      const output = "front-end";
+
+      const ctx = createTestContext({
+        ui: {
+          input: vi.fn(async () => output),
+          notify: vi.fn(),
+        },
+      });
+      rootPackResourceReducer("create", ctx);
+
+      expect(ctx.ui.input).toHaveBeenCalledWith("pack", "What is the name of your agent pack?");
+
+      expect(ctx.ui.input).toHaveResolvedWith(output);
+
+      expect(ctx.ui.notify).toHaveBeenCalledWith(
+        `Pack created successfully with name '${output}'`,
+      );
+    });
+
+    it("deletes a pack when delete is passed in", () => {
+      const output = "C#";
+
+      const ctx = createTestContext({
+        ui: {
+          input: vi.fn(async () => output),
+          notify: vi.fn(),
+        },
+      });
+      rootPackResourceReducer("delete", ctx);
+
+      expect(ctx.ui.input).toHaveBeenCalledWith(
+        "pack",
+        "What is the name of the pack you want to delete?",
+      );
+
+      expect(ctx.ui.input).toHaveResolvedWith(output);
+
+      expect(ctx.ui.notify).toHaveBeenCalledWith(
+        `Pack deleted successfully with name '${output}'`,
+      );
+    });
+  });
   describe.todo("Testing skillPackResourceReducer", () => {});
   describe.todo("Testing agentPackResourceReducer", () => {});
   describe.todo("Testing promptPackResourceReducer", () => {});
